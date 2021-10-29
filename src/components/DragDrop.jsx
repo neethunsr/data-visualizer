1c525f73c51a172e7edcf33fcde39cc41fa10c86
neethunsr <neethu2001nsr@gmail.com>
2021-10-29 14:47:52 +0530

    Use map for displaying list items

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,82 +1,134 @@
-import React, {useState} from 'react';
-import {ListGroup, Container, Row, Col} from 'react-bootstrap';
-import data from '../data';
-import '../dragdrop.css';
-import LineGraph from './LineGraph';
-import { Form } from 'react-bootstrap';
-import PieChart from './PieChart';
-import '../app.css';
+import React, { useState } from "react";
+import { ListGroup, Container, Row, Col } from "react-bootstrap";
+import data from "../data";
+import "../dragdrop.css";
+import LineGraph from "./LineGraph";
+import { Form } from "react-bootstrap";
+import PieChart from "./PieChart";
+import "../app.css";
 
 export const DragDrop = () => {
-  const [dragged, setDragged] = useState("")
-  const [x, setX] = useState('');
-  const [y, setY] = useState('');
+	const [dragged, setDragged] = useState("");
+	const [x, setX] = useState("");
+	const [y, setY] = useState("");
 
-  function allowDrop(event) {
-    event.stopPropagation();
-    event.preventDefault();
-  }
-  
-  function drag(event) {
-    setDragged(event.target.id);
-    console.log(event.target.draggable);
-    console.log(dragged);
-  }
+	function allowDrop(event) {
+		event.stopPropagation();
+		event.preventDefault();
+	}
 
-  function drop(event) {
-    if (event.target.id === "x")
-      setX(dragged) 
-    else if(event.target.id === "y")
-      setY(dragged)
-      // console.log("x",x);
-      // console.log("y",y);
-  }
+	function drag(event) {
+		setDragged(event.target.id);
+		console.log(event.target.draggable);
+		console.log(dragged);
+	}
 
-  function ChooseGraph(){
-    const [selected, setSelected] = useState("");
-    const [xLabel, setxLabel] = useState(null);
-    const [yLabel, setyLabel] = useState(null);
+	function drop(event) {
+		if (event.target.id === "x") setX(dragged);
+		else if (event.target.id === "y") setY(dragged);
+		// console.log("x",x);
+		// console.log("y",y);
+	}
 
-    const changeSelectOptionHandler = (event) => {
-        setSelected(event.target.value);
-        setxLabel(data[x]);
-        setyLabel(data[y]);
-    };
-    return(
-        <div className="choose">            
-            <div className="drop-down">
-            <Form.Select onChange={changeSelectOptionHandler} aria-label="Default select example">
-                <option>Choose a graph</option>
-                <option value="pie" >Pie Chart</option>
-                <option value="line">Line Chart</option>
-            </Form.Select>
-            </div>
-            {selected==="line" && <LineGraph xlabel={xLabel} ylabel={yLabel} label={x} title={x.concat("-",y," Chart")}/>}
-            {selected==="pie" && <PieChart xlabel={xLabel} ylabel={yLabel} title={x.concat("-",y, " Chart")}/>}
-        </div>
-    );
-  }
-  return (
-    <div className="">    
-    <ListGroup horizontal>
-      <ListGroup.Item variant="success" draggable="true" id={Object.keys(data)[0]} onDragStart={drag}>{Object.keys(data)[0]}</ListGroup.Item>
-      <ListGroup.Item variant="success" draggable="true" id={Object.keys(data)[1]} onDragStart={drag}>{Object.keys(data)[1]}</ListGroup.Item>
-    </ListGroup>
-    <ListGroup horizontal>
-      <ListGroup.Item variant="primary" draggable="true" id={Object.keys(data)[2]} onDragStart={drag}>{Object.keys(data)[2]}</ListGroup.Item>
-      <ListGroup.Item variant="primary" draggable="true" id={Object.keys(data)[3]} onDragStart={drag}>{Object.keys(data)[3]}</ListGroup.Item>
-      <ListGroup.Item variant="primary" draggable="true" id={Object.keys(data)[4]} onDragStart={drag}>{Object.keys(data)[4]}</ListGroup.Item>
-      <ListGroup.Item variant="primary" draggable="true" id={Object.keys(data)[5]} onDragStart={drag}>{Object.keys(data)[5]}</ListGroup.Item>
-    </ListGroup>
-    <Container style={{margin:"30px"}} fluid="sm-6">
-      <Row>
-        <Col className="dropBox categorical" id="x" onDrop={drop} onDragOver={allowDrop}><span>X/Categorical values: {x}</span></Col>
-        <Col className="dropBox numerical" id="y" onDrop={drop} onDragOver={allowDrop}><span>Y/Numerical values: {y}</span></Col>
-      </Row>
-    </Container>
-    <ChooseGraph/>
-    </div>
-  )
-}
+	function ChooseGraph() {
+		const [selected, setSelected] = useState("");
+		const [xLabel, setxLabel] = useState(null);
+		const [yLabel, setyLabel] = useState(null);
+
+		const changeSelectOptionHandler = (event) => {
+			setSelected(event.target.value);
+			setxLabel(data[x]);
+			setyLabel(data[y]);
+		};
+		return (
+			<div className="choose">
+				<div className="drop-down">
+					<Form.Select
+						onChange={changeSelectOptionHandler}
+						aria-label="Default select example"
+					>
+						<option>Choose a graph</option>
+						<option value="pie">Pie Chart</option>
+						<option value="line">Line Chart</option>
+					</Form.Select>
+				</div>
+				{selected === "line" && (
+					<LineGraph
+						xlabel={xLabel}
+						ylabel={yLabel}
+						label={x}
+						title={x.concat("-", y, " Chart")}
+					/>
+				)}
+				{selected === "pie" && (
+					<PieChart
+						xlabel={xLabel}
+						ylabel={yLabel}
+						title={x.concat("-", y, " Chart")}
+					/>
+				)}
+			</div>
+		);
+	}
+	let listItems = Object.keys(data);
+	// console.log(listItems);
+	return (
+		<div className="">
+			{/* <ListGroup horizontal>
+				{listItems.map(
+					(item, index) => (
+						<ListGroup.Item
+							variant="success"
+							draggable="true"
+							id={item}
+							key={index}
+							onDragStart={drag}
+						>
+							{item}
+						</ListGroup.Item>
+					)
+					// console.log(item)
+				)}
+			</ListGroup> */}
+			<ListGroup horizontal>
+				{listItems.map(
+					(item, index) => (
+						<ListGroup.Item
+							variant="primary"
+							draggable="true"
+							id={item}
+							key={index}
+							onDragStart={drag}
+						>
+							{item}
+						</ListGroup.Item>
+					)
+					// console.log(item)
+				)}
+			</ListGroup>
+			<Container style={{ margin: "30px" }} fluid="sm-6">
+				<Row>
+					<Col
+						className="dropBox categorical"
+						id="x"
+						onDrop={drop}
+						onDragOver={allowDrop}
+					>
+						<span>X/Categorical values: {x}</span>
+					</Col>
+					<Col
+						className="dropBox numerical"
+						id="y"
+						onDrop={drop}
+						onDragOver={allowDrop}
+					>
+						<span>Y/Numerical values: {y}</span>
+					</Col>
+				</Row>
+			</Container>
+			<ChooseGraph />
+		</div>
+	);
+};
 
 export default DragDrop;
--- src/data.js
+++ src/data.js
@@ -1,3 +1,14 @@
+// const categoricalData = {
+//     Days : ['Sun', 'Mon', 'Tues', 'Wednes', 'Thurs', 'Fri', 'Sat'],
+//     Months : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+// }
+// const numericalData = {
+//     Expense: [10, 20, 30, 40, 50, 0, 70],
+//     Income: [20, 40, 60, 80, 90, 120, 140],
+//     Present : [25, 10, 34, 40, 20, 18, 36, 24, 34, 40, 9, 17],
+//     Absent : [15, 30, 6, 0, 20, 22, 4, 16, 6, 0, 31, 23],
+// }
+
 const data = {
     Days : ['Sun', 'Mon', 'Tues', 'Wednes', 'Thurs', 'Fri', 'Sat'],
     Months : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -7,4 +18,5 @@ const data = {
     Absent : [15, 30, 6, 0, 20, 22, 4, 16, 6, 0, 31, 23],
 }
 
+// export {categoricalData, numericalData};
 export default data;
